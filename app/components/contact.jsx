6bb8b54478e68edf6698199e1e4d7604b337b168
Solomon Bonney <quophiclef@gmail>
2024-02-26 12:46:53 +0000

    debugged emailjs

--- app/components/contact.jsx
+++ app/components/contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FormValidation from "../validation/formValidation";
 import { Form, ErrorMessage, Field, Formik } from "formik";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import Link from "next/link";
 import {
   BsFacebook,
