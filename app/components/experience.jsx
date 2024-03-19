e9a9bb80c431fcfa689d31f3e270f11766445271
Solomon Bonney <quophiclef@gmail>
2024-03-19 18:46:26 +0000

    removed works

--- app/components/experience.jsx
+++ app/components/experience.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import jssvg from "../../public/javascript.svg";
-import bootstrapsvg from "../../public/bootstrap-svg.svg";
 import reactsvg from "../../public/react-svg.svg";
 import mongodbsvg from "../../public/mongodb-svgrepo-com.svg";
 import nextjssvg from "../../public/nextjs-fill-svgrepo-com.svg";
 import reduxsvg from "../../public/redux-svg.svg";
 import Image from "next/image";
+import { motion } from "framer-motion";
 const Experience = () => {
   return (
-    <div className="container-fluid shadow experience">
+    <motion.div
+      initial={{ rotate: 0 }}
+      whileInView={{ rotate: 360 }}
+      transition={{ type: "spring", stiffness: 260, damping: 20 }}
+      className="container-fluid shadow experience"
+    >
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 text-light d-flex align-items-center text-center mt-3">
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 text-light d-flex align-items-center text-center mt-4">
             <h1 className="fs-1 fw-bold me-2">3+</h1>
             <p className="p text-uppercase">YEARS OF EXPERIENCE</p>
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 flex-fill h-100 d-flex py-2 text-light align-items-center text-center mt-3">
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 flex-fill h-100 d-flex py-2 text-light align-items-center text-center mt-4">
             <h1 className="fs-1 fw-bold me-2">15+</h1>
             <p className="p text-uppercase">PROJECTS COMPLETED</p>
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 flex-fill h-100 d-flex py-2 mb-4 mt-2">
-            <Image
-              src={bootstrapsvg}
-              alt="an svg for bootstrap"
-              className="img-fluid bootstrap-svg m-2"
-            />
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 flex-fill h-100 d-flex py-2 mb-4 mt-3">
             <Image
               src={jssvg}
               alt="an svg for javascript"
@@ -53,7 +53,7 @@ const Experience = () => {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
